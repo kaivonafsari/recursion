eab9c2f8abac15cdd22f449cd848f247667f84d4
kaivonafsari <kaivon.afsari@gmail.com>
2015-03-02 16:14:49 -0800

    Finish Recusion

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,30 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
+var getElementsByClassName = function (className) {
   // your code here
+  var result = []
+  var getElement = function(nodes){
+  	if (nodes !== undefined) {
+  		var ClassList = nodes.classList
+  		if (nodes.length) {
+  			for (var i =0; i < nodes.length; i++) {
+  				if (nodes[i].classList !== undefined) {
+  					getElement(nodes[i])
+  				}
+  			}
+  		}
+
+  		if (ClassList) {
+  			if (ClassList.contains(className)){
+  				result.push(nodes)
+  			}
+  			getElement(nodes.childNodes)
+  		}
+  	}
+  };
+
+  getElement(document.body)
+  return result
+  	
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,36 @@
 
 // but you don't so you're going to write it from scratch:
 
-var stringifyJSON = function(obj) {
+var stringifyJSON = function (obj) {
   // your code goes here
+  if (obj===null){
+    return "null";
+  }
+  else if (obj === undefined){
+    return "undefined";
+  }
+  else if (typeof(obj)==="string"){
+    return '"'+obj+'"';
+  }
+  else if (Array.isArray(obj)) {
+    var yolo=""
+
+    for (var i=0;i<obj.length;i++){
+      yolo=yolo.concat(stringifyJSON(obj[i])+',');
+    }
+    return '['+yolo.slice(0,yolo.length-1)+']'; 
+  }
+  else if (typeof(obj)==="object"){   
+    var solo="";
+    for (var key in obj){
+      if (obj[key] !== undefined && typeof(obj[key])!=="function"){
+        solo=solo.concat(stringifyJSON(key)+':'+stringifyJSON(obj[key])+',');
+      }
+    }
+    return '{'+solo.slice(0,solo.length-1)+'}'; 
+  }
+  else{
+    return obj.toString();
+  }
+
 };
